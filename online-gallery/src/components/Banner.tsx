25fbc60938638f59a96725fa704f5db5fc8d674e
s-ggul <k0s0a7@gmail.com>
2025-07-21 11:00:49 +0900

    이미지 내장방식으로 LCP 개선(api호출 제거)

--- online-gallery/src/components/Banner.tsx
+++ online-gallery/src/components/Banner.tsx
@@ -1,25 +1,25 @@
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
 
-const fetchBanner = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  const res = await fetch("/api/banner.json");
-  if (!res.ok) throw new Error("Network response was not ok");
-  return res.json();
-};
+// const fetchBanner = async () => {
+//   await new Promise((resolve) => setTimeout(resolve, 500));
+//   const res = await fetch("/api/banner.json");
+//   if (!res.ok) throw new Error("Network response was not ok");
+//   return res.json();
+// };
 
 function Banner() {
-  const { data: banner } = useQuery({
-    queryKey: ["banner"],
-    queryFn: fetchBanner,
-  });
+  // const { data: banner } = useQuery({
+  //   queryKey: ["banner"],
+  //   queryFn: fetchBanner,
+  // });
+
+  const banner = "/event-banner.webp"; // 배너 이미지를 서버에서 불러오는게 아닌 바로 가져오도록
 
   if (!banner) return null;
 
   return (
     <div className="w-full bg-[#1a2a3a] flex justify-center items-center mb-14 h-[500px]">
-      {banner && (
-        <img src={banner?.banner} alt="Event Banner" className="block h-full" />
-      )}
+      <img src={banner} alt="Event Banner" className="block h-full" />
     </div>
   );
 }
