648b2fc26c492309acce96ace10ef0859df9b989
s-ggul <k0s0a7@gmail.com>
2025-07-22 11:39:45 +0900

    font preload 및 script difer 관련

--- wedding-card/vite.config.ts
+++ wedding-card/vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import UnpluginInjectPreload from "unplugin-inject-preload/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    UnpluginInjectPreload({
+      files: [
+        {
+          entryMatch: /.*\.woff2$/,
+          attributes: {
+            crossorigin: "anonymous",
+          },
+        },
+        // {
+        //   outputMatch: /.*.(css|js)$/,
+        // },
+      ],
+    }),
+  ],
 });
